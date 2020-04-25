d0eefa6b1baa0b5a22031ebbed596c7e5cb30c4a
jason tlh <jasonjdh94@gmail.com>
2020-04-25 18:51:36 -0400

    completed assessment

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const CSVtoJSON = require('csvtojson');
+const fs = require('fs')
 
 const convert = (path) => {
   return new Promise((resolve, reject) => {
@@ -18,14 +19,23 @@ convert(process.argv[4]),
 convert(process.argv[5])])
   .then(res => {
     const dataObj = {
-      courses: { ...res[0] },
-      students: { ...res[1] },
-      tests: { ...res[2] },
+      courses: {},
+      students: {},
+      tests: {},
       marks: { ...res[3] }
     }
+    res[0].forEach(course => {
+      dataObj.courses[course.id] = course
+    })
+    res[1].forEach(student => {
+      dataObj.students[student.id] = student
+    })
+    res[2].forEach(test => {
+      dataObj.tests[test.id] = test
+    })
 
     Object.values(dataObj.students).forEach(student => {
-      dataObj.students[student.id - 1].id = Number(dataObj.students[student.id - 1].id)
+      dataObj.students[student.id].id = Number(dataObj.students[student.id].id)
       const marksFilter = Object.values(dataObj.marks).filter((mark) => mark.student_id == student.id)
 
       // course averages
@@ -49,19 +59,20 @@ convert(process.argv[5])])
 
       // total average
       const totalAvg = Object.values(averages).reduce((prev, curr) => prev + curr.average, 0) / Object.values(averages).length
-      dataObj.students[student.id - 1].totalAverage = Number(Number(totalAvg).toFixed(2))
+      dataObj.students[student.id].totalAverage = Number(Number(totalAvg).toFixed(2))
 
-      dataObj.students[student.id - 1].courses = []
+      dataObj.students[student.id].courses = []
       Object.values(averages).forEach(average => {
-        dataObj.students[student.id - 1].courses.push(average)
+        dataObj.students[student.id].courses.push(average)
       })
     })
 
     const final = {
       students: Object.values(dataObj.students)
     }
-
-    console.log(final)
+    fs.writeFile(process.argv[6], JSON.stringify(final), (err) => {
+      if (err) throw err
+    })
   })
   .catch(error => {
     console.error(error)
